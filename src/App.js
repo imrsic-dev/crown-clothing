41717946a86dcf305e9a0a52c04fd0d6a9e9285f
imrsic-dev <ivan.mrsic.dev@gmail.com>
2022-08-30 14:42:56 +0200

    setup categories

--- src/App.js
+++ src/App.js
@@ -1,52 +1,40 @@
 const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+    },
+    {
+      id: 4,
+      title: "Womens",
+    },
+    {
+      id: 5,
+      title: "Mans",
+    },
+  ];
   return (
-    <>
-      <div className="categories-container">
-        <div className="category-container">
-          {/* <img src="" alt="" /> */}
-          <div className="category-body-container">
-            <h2>Hats</h2>
-            <p>Shop now</p>
+    <div className="categories-container">
+      {categories.map(({ id, title }) => {
+        return (
+          <div className="category-container">
+            {/* <img src="" alt="" /> */}
+            <div key={id} className="category-body-container">
+              <h2>{title}</h2>
+              <p>Shop now</p>
+            </div>
           </div>
-        </div>
-      </div>
-      <div className="categories-container">
-        <div className="category-container">
-          {/* <img src="" alt="" /> */}
-          <div className="category-body-container">
-            <h2>Jackets</h2>
-            <p>Shop now</p>
-          </div>
-        </div>
-      </div>
-      <div className="categories-container">
-        <div className="category-container">
-          {/* <img src="" alt="" /> */}
-          <div className="category-body-container">
-            <h2>Sneakers</h2>
-            <p>Shop now</p>
-          </div>
-        </div>
-      </div>
-      <div className="categories-container">
-        <div className="category-container">
-          {/* <img src="" alt="" /> */}
-          <div className="category-body-container">
-            <h2>Womens</h2>
-            <p>Shop now</p>
-          </div>
-        </div>
-      </div>
-      <div className="categories-container">
-        <div className="category-container">
-          {/* <img src="" alt="" /> */}
-          <div className="category-body-container">
-            <h2>Mans</h2>
-            <p>Shop now</p>
-          </div>
-        </div>
-      </div>
-    </>
+        );
+      })}
+    </div>
   );
 };
 
