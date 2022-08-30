ffd3e3fbcd46c0231bcaa60a9c078ac4e55b8c63
imrsic-dev <ivan.mrsic.dev@gmail.com>
2022-08-30 14:10:53 +0200

    cleanup, scaffolding capstone project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="categories-container">
+        <div className="category-container">
+          {/* <img src="" alt="" /> */}
+          <div className="category-body-container">
+            <h2>Hats</h2>
+            <p>Shop now</p>
+          </div>
+        </div>
+      </div>
+      <div className="categories-container">
+        <div className="category-container">
+          {/* <img src="" alt="" /> */}
+          <div className="category-body-container">
+            <h2>Jackets</h2>
+            <p>Shop now</p>
+          </div>
+        </div>
+      </div>
+      <div className="categories-container">
+        <div className="category-container">
+          {/* <img src="" alt="" /> */}
+          <div className="category-body-container">
+            <h2>Sneakers</h2>
+            <p>Shop now</p>
+          </div>
+        </div>
+      </div>
+      <div className="categories-container">
+        <div className="category-container">
+          {/* <img src="" alt="" /> */}
+          <div className="category-body-container">
+            <h2>Womens</h2>
+            <p>Shop now</p>
+          </div>
+        </div>
+      </div>
+      <div className="categories-container">
+        <div className="category-container">
+          {/* <img src="" alt="" /> */}
+          <div className="category-body-container">
+            <h2>Mans</h2>
+            <p>Shop now</p>
+          </div>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
