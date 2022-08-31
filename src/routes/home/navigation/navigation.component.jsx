58c38d2f3cc44cab7e000f3037af8da29d08a596
imrsic-dev <ivan.mrsic.dev@gmail.com>
2022-08-31 19:47:29 +0200

    add logo, style navigation

---
+++ src/routes/home/navigation/navigation.component.jsx
@@ -0,0 +1,26 @@
+import { Outlet, Link } from "react-router-dom";
+import { ReactComponent as CrownLogo } from "../../../assets/crown.svg";
+
+import "./navigation.styles.scss";
+
+const Navigation = () => {
+  return (
+    <>
+      <div className="navigation">
+        <Link to="/">
+          <div className="logo-container">
+            <CrownLogo className="logo" />
+          </div>
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </>
+  );
+};
+
+export default Navigation;
