309a484e9e8f35535e949bc7c50c5c5095185e78
imrsic-dev <ivan.mrsic.dev@gmail.com>
2022-08-31 20:01:04 +0200

    set up firebase, create sign-in route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./routes/home/home.component";
-import Navigation from "./routes/home/navigation/navigation.component";
+import Navigation from "./routes/navigation/navigation.component";
+import SignIn from "./routes/sign-in/sign-in.component";
 
 const Shop = () => {
   return <div>SHOP</div>;
@@ -12,7 +13,8 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path="/shop" element={<Shop />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,5 +1,5 @@
 import { Outlet, Link } from "react-router-dom";
-import { ReactComponent as CrownLogo } from "../../../assets/crown.svg";
+import { ReactComponent as CrownLogo } from "../../assets/crown.svg";
 
 import "./navigation.styles.scss";
 
@@ -15,6 +15,9 @@ const Navigation = () => {
         <div className="nav-links-container">
           <Link className="nav-link" to="/shop">
             SHOP
+          </Link>
+          <Link className="nav-link" to="/sign-in">
+            SIGN IN
           </Link>
         </div>
       </div>
